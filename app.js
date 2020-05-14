7e0ef874eee9065b6ae070e0eb23fb72fa8cda3e
sharatsajjan <37244600+sharatsajjan@users.noreply.github.com>
2020-05-15 02:40:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,12 +10,6 @@ app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
  
-var connection = mysql.createConnection({
-  host     : '127.0.0.1',
-  user     : 'fusionclicks',
-  database : 'user'
-  password : 'sharat8794'
-});
 
 app.get("/", function(req, res){
  var q = 'SELECT COUNT(*) as count FROM users';
